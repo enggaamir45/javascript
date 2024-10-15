92d3c822ecb8a59bb1f7d538615f7348951c1f84
Amir Husain Ansari <ar.ansari45@gmail.com>
2024-10-15 17:32:42 +0000

    config done

--- javascript-basics/intro-01.js
+++ javascript-basics/intro-01.js
@@ -1,1 +1,11 @@
 console.log("hamza")
+const accountId = 248236
+let accountName = "Hamza"
+let accountPassword = 12345
+accountCity = "Burhanpur"
+
+// accountId = 1234  We cannot change value of const
+
+console.log(accountName);
+
+console.table([accountId, accountName, accountPassword, accountCity])
---
+++ javascript-basics/intro-02.js
@@ -0,0 +1,22 @@
+let name = "Hamza"
+let age = 33
+let score = "33"
+let height = "33abc"
+
+// console.log(typeof age);
+// console.log(typeof score);
+// console.log(typeof height);
+
+let valueInNumber = Number(height)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+console.log(typeof null); // typeof null is object
+
+
+
+
+
+
+
